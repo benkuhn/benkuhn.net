a8a0150add492267002e7bf67fffe4a8a4003a82
Ben Kuhn <ben.s.kuhn@gmail.com>
2013-02-21 01:43:02 -0500

    add files

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+                       url(r'^tag/(?P<slug>[\w-]+)$', 'posts.views.tag'),
+                       url(r'^rss/$', 'posts.views.rss'),
+                       url(r'^archive/(?P<page>\d+)$', 'posts.views.archive'),
+                       url(r'^(?P<slug>[\w-]+)$', 'posts.views.by_slug'),
+                       )
